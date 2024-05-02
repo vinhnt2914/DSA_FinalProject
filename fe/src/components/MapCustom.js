52106f7e46b152273f2deb449899a6c45ffecbc3
Ronaldo93 <51114190+Ronaldo93@users.noreply.github.com>
2024-05-02 08:32:51 +0700

    feat: input now support multiple choice 📃
    * feat: changed to new UI library that supports multiple selection

--- fe/src/components/MapCustom.js
+++ fe/src/components/MapCustom.js
@@ -1,9 +1,10 @@
-import { useMap } from 'react-leaflet';
+import { useMap } from 'react-leaflet'
 
 export default function MapCustom({ lat, lng, zoom }) {
-    const map = useMap();
-    const center = [lat, lng]
-    map.setView(center, zoom);
-    // rerender the map
-    return null;
+  console.table('[mapcustom] lat, lng, zoom: ', lat, lng, zoom)
+  const map = useMap()
+  const center = [lat, lng]
+  map.setView(center, zoom)
+  // rerender the map
+  return null
 }
--- fe/src/components/ServiceList.js
+++ fe/src/components/ServiceList.js
@@ -1,38 +1,67 @@
-import { CiSearch } from "react-icons/ci";
-import React, { useState } from "react";
-import { Autocomplete, AutocompleteItem, Button } from "@nextui-org/react";
-import { serviceData } from "../data/data";
+import React, { useState } from 'react'
+// import { Autocomplete, AutocompleteItem, Button } from "@nextui-org/react";
+// import { Combobox } from '@headlessui/react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated'
+
+// DATA
+import { serviceData } from '../data/data'
+// import { divIcon } from "leaflet";
+
+// Animation
+const animatedComponents = makeAnimated()
 
 export default function ServiceList({ data: getData }) {
-    // Initialize the state
-    const [services, setServices] = useState("");
+  // Initialize the state
+  const [services, setServices] = useState('')
 
-    /**
-    * Fetches data.
-    * @throws {Error} When the function is not implemented.
-    */
-    function fetchData() {
-        
-        // Temporary implementation using 
-        // mock data
-        // TODO: Fetch using backend API
-        const sampleData = [[0, 0], [1, 1], [2, 2]];
-        getData(sampleData);
-    }
+  function handleChange(selection) {
+    // setServices(selectedOption)
+    console.log(selection)
+  }
 
-    return (
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4 items-center">
-            <Autocomplete
-                label="Select or type service name"
-                className="max-w-xs"
-            >
-                {serviceData.map((service) => (
-                    <AutocompleteItem key={service.value} value={service.value}>
-                        {service.label}
-                    </AutocompleteItem>
-                ))}
-            </Autocomplete>
-            <Button isIconOnly={true} onClick={fetchData}><CiSearch /></Button>
-        </div>
-    );
+  /**
+   * Fetches data.
+   * @throws {Error} - The function is not implemented.
+   */
+  function fetchData() {
+    // Temporary implementation using
+    // mock data
+    // TODO: Fetch using backend API
+    const sampleData = [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ]
+    getData(sampleData)
+  }
+
+  return (
+    // <div className="flex w-full flex-wrap md:flex-nowrap gap-4 items-center">
+    //     <Autocomplete
+    //         label="Select or type service name"
+    //         className="max-w-xs"
+    //     >
+    //         {serviceData.map((service) => (
+    //             <AutocompleteItem key={service.value} value={service.value}>
+    //                 {service.label}
+    //             </AutocompleteItem>
+    //         ))}
+    //     </Autocomplete>
+    //     <Button isIconOnly={true} onClick={fetchData}><CiSearch /></Button>
+    // </div>
+    <div className="w-80 ">
+      <Select
+        //   defaultValue={}
+        isMulti
+        components={animatedComponents}
+        name="colors"
+        placeholder="Search for services..."
+        options={serviceData}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        onChange={handleChange}
+      />
+    </div>
+  )
 }
