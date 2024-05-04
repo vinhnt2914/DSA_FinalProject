8fe456ac5a772a433bccbde29de91f716d08d011
Andrew <57421747+andrewcoldbrew@users.noreply.github.com>
2024-05-04 14:53:36 +0700

    Update POIController.java

--- src/main/java/com/example/testapi/controller/POIController.java
+++ src/main/java/com/example/testapi/controller/POIController.java
@@ -13,58 +13,50 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class POIController {
 
-    @GetMapping("/get/{x}/{y}")
-    public ResponseEntity<POIJson> getPOI(@PathVariable int x,
-                          @PathVariable int y){
-        POI poi = APIDataManager.getInstance().poiHashMap.find(x,y);
+    private final APIDataManager apiDataManager;
 
+    public POIController() {
+        this.apiDataManager = APIDataManager.getInstance(); 
+    }
+
+    @GetMapping("/get/{x}/{y}")
+    public ResponseEntity<POIJson> getPOI(@PathVariable int x, @PathVariable int y) {
+        POI poi = apiDataManager.poiHashMap.find(x, y);
         if (poi == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return ResponseEntity.ok(poi.mapToPOIJson());
     }
 
     @GetMapping()
-    public List<POIWithDistanceJson> getManyPOIs(){
-        APIDataManager apiDataManager = APIDataManager.getInstance();
+    public List<POIWithDistanceJson> getManyPOIs() {
         List<POIWithDistance> poiList = apiDataManager.poiWithDistanceList;
         return poiList.stream().map(POIWithDistance::mapToJSON).toList();
     }
 
     @PostMapping()
     public ResponseEntity<List<POIWithDistanceJson>> searchPOIs(@RequestBody Map<String, String> body) {
-        // Get the params from request body
         int x = Integer.parseInt(body.get("x"));
         int y = Integer.parseInt(body.get("y"));
         String service = body.get("service");
-
-        // Get the POIWithDistance from kdTree KNN search
-        List<POIWithDistance> poiList = APIDataManager.getInstance().kdTree.kNearestNeighborsWithMap(x,y,service).toArrayList();
-        // Convert to JSON format and return them
+        List<POIWithDistance> poiList = apiDataManager.kdTree.kNearestNeighborsWithMap(x, y, service).toArrayList();
         return new ResponseEntity<>(poiList.stream().map(POIWithDistance::mapToJSON).toList(), HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{x}/{y}")
-    public ResponseEntity<POIJson> updatePOI(@PathVariable int x,
-                             @PathVariable int y,
-                             @RequestBody Map<String, String[]> body) {
-        POI poi = APIDataManager.getInstance().poiHashMap.find(x,y);
-
+    public ResponseEntity<POIJson> updatePOI(@PathVariable int x, @PathVariable int y, @RequestBody Map<String, String[]> body) {
+        POI poi = apiDataManager.poiHashMap.find(x, y);
         if (poi == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
         MyArray<String> newServices = new MyArray<>(body.get("services"));
         poi.setServices(newServices);
         return new ResponseEntity<>(poi.mapToPOIJson(), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/remove/{x}/{y}")
-    public ResponseEntity<POIJson> removePOI(@PathVariable int x,
-                                             @PathVariable int y) {
-        APIDataManager apiDataManager = APIDataManager.getInstance();
+    public ResponseEntity<POIJson> removePOI(@PathVariable int x, @PathVariable int y) {
         POI poi = apiDataManager.poiHashMap.find(x, y);
         if (poi == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -80,8 +72,7 @@ public class POIController {
         String[] services = body.get("service").split(",");
         MyArray<String> newServices = new MyArray<>(services);
         POI poi = new POI(x, y, newServices);
-        APIDataManager.getInstance().poiHashMap.put(poi);
+        apiDataManager.poiHashMap.put(poi);
         return new ResponseEntity<>(poi.mapToPOIJson(), HttpStatus.CREATED);
     }
-
 }
