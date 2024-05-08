9afa22b06d0d361fdf588222ec8abc995aec428f
vinhnt2914 <113036284+vinhnt2914@users.noreply.github.com>
2024-05-08 22:41:11 +0700

    remove collections from APIDataManager

--- src/main/java/com/example/testapi/utility/APIDataManager.java
+++ src/main/java/com/example/testapi/utility/APIDataManager.java
@@ -6,30 +6,14 @@ import com.example.testapi.model.Map2D.Map2D;
 import com.example.testapi.model.POI;
 import com.example.testapi.model.POIWithDistance;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class APIDataManager {
     private static APIDataManager single_APIDataManager = null;
-    public List<POI> poiList;
-    public List<POIWithDistance> poiWithDistanceList;
     public KDTree kdTree;
     public Map2D poiHashMap;
 
     private APIDataManager() {
-        this.poiList = new ArrayList<>();
-        this.poiWithDistanceList = new ArrayList<>();
         DataManager.getInstance().createKDTreeAndMap(1000000);
         kdTree = DataManager.getInstance().kdTree;
-    }
-
-
-    public void populatePOI(MyArray<POI> poiArr) {
-        poiList = poiArr.toArrayList();
-    }
-
-    public void populatePOIWithDistance(MyArray<POIWithDistance> poiWithDistanceArr) {
-        poiWithDistanceList = poiWithDistanceArr.toArrayList();
     }
 
     public static synchronized APIDataManager getInstance()
